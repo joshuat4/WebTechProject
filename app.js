4ae811c4903fc7968fbdfaf18dca42a95f1545a1
Josh <joshuat4@student.unimelb.edu.au>
2018-03-28 00:01:32 +1100

    Made my own Branch to test things.

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //For Webtech
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-var app = express();
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
