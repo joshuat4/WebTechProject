4138168f175d3449ad33d988cdc8bd34546eb094
Kevin <hcheong1@student.unimelb.edu.au>
2018-03-28 00:02:40 +1100

    test3

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+var routes = require('./routes.js');
+
+
+app.use('/routes', routes);
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
