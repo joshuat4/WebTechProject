4cd34fc5f84fa5375f106439e0fc91a1405ca5cc
Josh <joshuat4@student.unimelb.edu.au>
2018-03-27 23:52:38 +1100

    Line 7: Assigned express function to var app.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+var app = express();
+
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
 });
