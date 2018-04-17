3c237b0fa774fedc78ca7b6454e82660dd536f67
Kevin <hcheong1@student.unimelb.edu.au>
2018-04-18 07:11:06 +1000

    test2

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.get('/collectionpartner.html', (req, res) => res.sendFile(path.join(__dirnam
 app.get('/maps.html', (req, res) => res.sendFile(path.join(__dirname + '/maps.html')));
 app.get('/RequestForm.html', (req, res) => res.sendFile(path.join(__dirname + '/RequestForm.html')));
 app.get('/impactpage.html', (req, res) => res.sendFile(path.join(__dirname + '/impactpage.html')));
-app.get('Icons and Images/banner.jpg', (req, res) => res.sendFile(path.join(__dirname + 'Icons and Images/banner.jpg')));
+app.get('/Icons%20and%20Images/banner.jpg', (req, res) => res.sendFile(path.join(__dirname + '/Icons%20and%20Images/banner.jpg')));
+app.get('/Icons%20and%20Images/info.png', (req, res) => res.sendFile(path.join(__dirname + '/Icons%20and%20Images/info.png')));
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
