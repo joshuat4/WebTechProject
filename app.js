901b591372470c8868f5ec295ce6c0f16a14baa8
Kevin <hcheong1@student.unimelb.edu.au>
2018-04-18 02:32:33 +1000

    testing app routing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/frontpage.html')));
 
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
