9fc1bf11fc779f6c4beccfd79679c777602c1105
Kevin <hcheong1@student.unimelb.edu.au>
2018-05-07 21:14:57 +1000

    lul

---
+++ models/customerData.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var customerDataSchema = new Schema(
+    {
+        NAME: {type: String, required: true},
+        EMAIL: {type: String, required: true},
+        PHONE: {type: String , required: false},
+        ADDRESS_LINE_1: {type: String, required: true},
+        ADDRESS_LINE_2: {type: String, required: false},
+        SUBURB: {type: String, required: true},
+        POSTCODE: {type: Number, required: true},
+        STATE: {type: String, required: true},
+        WASTE_TYPE: {type:String, enum:['Mobile Phones', 'Televisions', 'Batteries', 'Computers', 'Small Appliances', 'Large Appliances', 'Others'], required: true},
+        DESCRIPTION: {type: String, required: false}
+    }
+);
+
+//Export model
+module.exports = mongoose.model('customerDataInstance', customerDataSchema);
