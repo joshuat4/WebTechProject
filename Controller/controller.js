f5c7c89e69830ad3f5b9cce57e4fda6a9376718f
Josh <joshuat4@student.unimelb.edu.au>
2018-05-11 15:38:15 +1000

    Request redirects to new page which now also shows the updated numbers.

--- Controller/controller.js
+++ Controller/controller.js
@@ -3,9 +3,32 @@ var Stations = mongoose.model('geodata');
 var Customer = mongoose.model('customerDataInstance');
 const path = require('path');
 
+
+var countInArea = function(req, res){
+    console.log("countInArea Ran");
+    var inputPostcode = req.params.postcode;
+    var cust_length = 0;
+    var allInArea = Customer.find({POSTCODE : inputPostcode}, function(err, customers) {
+        if(!err){
+            console.log(customers.length);
+            cust_length =  customers.length;
+
+            res.send(customers);
+            return cust_length;
+
+        }else{
+            res.sendStatus(404);
+        }
+
+
+        // res.send(allInArea.count);
+    });
+};
+
+
 var findAllStations = function(req,res){
     var state = req.params.states;
-    console.log("function ran");
+    console.log("findAllStations ran");
     // var required_columns = "ADDITIONALINFO ADDRESS BUSINESSNAME BUSINESS_ID DESCRIPTION DROPOFF FEES GEO_LAT GEO_LON PHONE POSTCODE PROVIDER_NAME STATE SUBURB  WEBSITE Batteries Computers Electricals light_Globes Mobilephones Printer_catridges Televisions";
     // { 'STATE': state }, required_columns,
     Stations.find({ 'STATE': state },function(err,geodata){
@@ -19,7 +42,7 @@ var findAllStations = function(req,res){
 
 var findCloseStations = function(req,res){
     var coords = req.params;
-    console.log("function ran");
+    console.log("findCloseStations ran");
     console.log(coords);
 
     Stations.find({ loc :
@@ -36,6 +59,9 @@ var findCloseStations = function(req,res){
     });
 };
 
+
+
+
 // var findOneCafe = function(req,res){
 //     var cafeInx = req.params.id;
 //     Cafe.findById(cafeInx,function(err,cafe){
@@ -48,17 +74,6 @@ var findCloseStations = function(req,res){
 // };
 
 //For RequestForm/CheckRequests.
-var countInArea = function(inputPostcode, res){
-    var cust_length = 0;
-    var allInArea = Customer.find({POSTCODE : inputPostcode}, function(err, customers) {
-
-        console.log(customers.length);
-
-        cust_length =  customers.length;
-        // res.send(allInArea.count);
-    });
-    return cust_length;
-};
 
 var saveCustomer = function(req, res){
     console.log("got customer form data! saving to database.");
@@ -68,8 +83,9 @@ var saveCustomer = function(req, res){
 
     currCustomer.save()
     .then(customer => {
-        res.status(200).sendFile(path.join(__dirname + '/checkRequests.html'));
-        countInArea(currCustomer.POSTCODE,res);
+        // res.status(200).sendFile(path.join(__dirname + '/checkRequests.html'));
+        res.status(200).sendFile(path.join(__dirname + '/deliveryTracker.html'));
+        // countInArea("/api/postcode/" + currCustomer.POSTCODE,res);
     })
     .catch(err => {
         console.log("Error: " + err);
