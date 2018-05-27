23a97e83ff5d68abb03788640bac21b9518def8f
Hon Khay Kevin Cheong <h.cheong2@student.unimelb.edu.au>
2018-05-28 05:12:52 +1000

    final version

--- app.js
+++ app.js
@@ -15,18 +15,6 @@ const path = require('path');
 
 // move all those *.html files to ./static/
 app.use(express.static('htmlPages'));
-// app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/frontpage.html')));
-// app.get('/frontpage.html', (req, res) => res.sendFile(path.join(__dirname + '/frontpage.html')));
-// app.post('/frontpage.html', (req, res) => res.sendFile(path.join(__dirname + '/frontpage.html')));
-// app.get('/collectionpartner.html', (req, res) => res.sendFile(path.join(__dirname + '/collectionpartner.html')));
-// app.get('/maps.html', (req, res) => res.sendFile(path.join(__dirname + '/maps.html')));
-// app.get('/RequestForm.html', (req, res) => res.sendFile(path.join(__dirname + '/RequestForm.html')));
-// app.get('/impactpage.html', (req, res) => res.sendFile(path.join(__dirname + '/impactpage.html')));
-// app.get('/partnerjoinform.html', (req, res) => res.sendFile(path.join(__dirname + '/partnerjoinform.html')));
-// app.get('/howtohelp.html', (req, res) => res.sendFile(path.join(__dirname + '/howtohelp.html')));
-// app.get('/sowhat.html', (req, res) => res.sendFile(path.join(__dirname + '/sowhat.html')));
-// app.get('/whatEwaste.html', (req, res) => res.sendFile(path.join(__dirname + '/whatEwaste.html')));
-// app.get('/Controller/deliveryTracker.html', (req, res) => res.sendFile(path.join(__dirname + '/Controller/deliveryTracker.html')));
 
 
 // Database setup
@@ -35,9 +23,6 @@ require('./models/db.js');
 var routes = require('./routes/routes.js');
 
 
-
-//DONT DELETE ( PROLLY SHOULD MOVE TO CONTROLLER THOUGH I DONT KNOW HOW)
-//CUSTOMER
 app.use(bodyParser.urlencoded({/**/
     extended: true
 }));
@@ -47,23 +32,6 @@ app.use(bodyParser.json());
 app.use('/', routes);
 
 
-// var CustomerDataInstance = mongoose.model('customerDataInstance');
-// app.post('/post', function(req, res){
-//     console.log("got customer form data! saving to database.");
-//     var currCustomer = new Customer(req.body);
-//
-//     currCustomer.save(function (err) {
-//         if (err) return handleError(err);
-//     });
-//
-//     var count = CustomerDataInstance.countInArea(3000,res);
-//
-//     console.log("countInArea: " + count);
-//
-//     res.status(200).sendFile(path.join(__dirname + '/checkRequests.html'));
-// });
-
-
 //PARTNER
 app.post('/partnerpost', function(req, res){
     console.log("got partner form data! saving to database.");
@@ -71,7 +39,6 @@ app.post('/partnerpost', function(req, res){
     currPartner.save(function (err) {
         if (err) return handleError(err);
     });
-    // res.status(200).sendFile(path.join(__dirname + '/frontpage.html'));
 });
 
 
@@ -81,10 +48,3 @@ app.post('/partnerpost', function(req, res){
 app.listen(PORT, function(){
     console.log(`Express listening on port ${PORT}`);
 });
-
-// var mongoose = require('mongoose');
-// var mongoDB = 'mongodb://hcheong1:qwer1234!@ds111050.mlab.com:11050/tute9';
-// mongoose.connect(mongoDB);
-// mongoose.Promise = global.Promise;
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
